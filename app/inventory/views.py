0f10fe3538cea39d51f6e741667b3c07ed8fb335
victortys <43775121+victortys@users.noreply.github.com>
2019-06-03 12:58:22 +0800

    inventory fields update

--- app/inventory/views.py
+++ app/inventory/views.py
@@ -1,7 +1,10 @@
 from flask import render_template, url_for, redirect, flash, request, json, jsonify
 from . import inventory
+import os
+import datetime
 from forms import InventoryForm
 from flask_login import login_required
+from werkzeug import secure_filename
 
 from app import db
 from ..models import Inventory, Serializer
@@ -16,9 +19,15 @@ def save_item():
     if request.method == 'POST':
         pid = request.form.get('pid')
         name = request.form.get('name')
+        description = request.form.get('description')
+        code = request.form.get('code')
+        material = request.form.get('material')
         price = request.form.get('price')
         quantity = request.form.get('quantity')
-        item = Inventory(pid, name, price, quantity)
+        perbox = request.form.get('perbox')
+        location = request.form.get('location')
+        file = request.form.get('file')
+        item = Inventory(pid, name, description, code, material, price, quantity, perbox, location, file, created_date=datetime.datetime.now())
         db.session.add(item)
         db.session.commit()
         return json.dumps(item.serialize()), 200
@@ -45,13 +54,26 @@ def update_items(pid):
     """
     item = Inventory.query.get_or_404(pid)
     name = request.form.get('name')
+    description = request.form.get('description')
+    code = request.form.get('code')
+    material = request.form.get('material')
     price = request.form.get('price')
     quantity = request.form.get('quantity')
+    perbox = request.form.get('perbox')
+    location = request.form.get('location')
+    file = request.form.get('file')
 
     # update changes
     item.name = name
+    item.description = description
+    item.code = code
+    item.material = material
     item.price = price
     item.quantity = quantity
+    item.perbox = perbox
+    item.location = location
+    item.file = file
+    item.updated_date = datetime.datetime.now()
     db.session.commit()
 
     return json.dumps(item.serialize()), 200
@@ -69,5 +91,21 @@ def delete_items(pid):
     return jsonify("Item deleted"), 200
 
 
+@inventory.route("/upload_image/<int:pid>", methods=['POST'])
+@login_required
+def upload_images(pid):
+    """
+    Upload images
+    """
+    pic = request.files['file']
+    if pic.filename != '':
+        pic_dir = os.path.join(os.path.abspath(os.curdir)) + "/", pic
+        pic.save(pic_dir)
+
+        item = Inventory.query.get_or_404(pid)
+        item.filename = pic_dir
+        db.session.commit()
+
+    return jsonify('File uploaded successfully'), 200
 
 
