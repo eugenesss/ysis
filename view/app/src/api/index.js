2ebbd74ccf31f78b517663d1520b723d915df523
Eugene Sim <eugene@ocdigitalnetwork.com>
2019-07-27 00:31:24 +0800

    merged

--- view/app/src/api/index.js
+++ view/app/src/api/index.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-  //  baseURL: "http://localhost:5000/",
-  baseURL: "http://178.128.84.254:8000/",
-=======
   baseURL: "http://178.128.84.254:8000/",
   //baseURL: "http://localhost:5000/",
->>>>>>> a2160e339819201a2221dd91a0e6e709a966bb95
   timeout: 8000
 });
 
